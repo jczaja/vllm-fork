dc5d19247ee0db9cfb3bbaff7f9c2031fcd00a0a
Jacek Czaja <jczaja@habana.ai>
2024-11-26 14:25:42 +0200

    Draft of profiler

--- vllm/entrypoints/llm.py
+++ vllm/entrypoints/llm.py
@@ -44,6 +44,8 @@ from vllm.transformers_utils.tokenizer_group import TokenizerGroup
 from vllm.usage.usage_lib import UsageContext
 from vllm.utils import Counter, deprecate_args, deprecate_kwargs, is_list_of
 
+from torch import profiler
+
 logger = init_logger(__name__)
 
 _R = TypeVar("_R", default=Any)
@@ -388,6 +390,7 @@ class LLM:
         guided_options_request: Optional[Union[LLMGuidedOptions,
                                                GuidedDecodingRequest]] = None,
         priority: Optional[List[int]] = None,
+        profiling : bool = False,
     ) -> List[RequestOutput]:
         """Generates the completions for the input prompts.
 
@@ -466,7 +469,7 @@ class LLM:
             guided_options=guided_options_request,
             priority=priority)
 
-        outputs = self._run_engine(use_tqdm=use_tqdm)
+        outputs = self._run_engine(use_tqdm=use_tqdm, profiling=profiling)
         return self.engine_class.validate_outputs(outputs, RequestOutput)
 
     def collective_rpc(self,
@@ -1376,7 +1379,7 @@ class LLM:
         return params
 
     def _run_engine(
-            self, *, use_tqdm: bool
+            self, *, use_tqdm: bool, profiling : bool = False,
     ) -> List[Union[RequestOutput, PoolingRequestOutput]]:
         # Initialize tqdm.
         if use_tqdm:
@@ -1393,8 +1396,20 @@ class LLM:
         outputs: List[Union[RequestOutput, PoolingRequestOutput]] = []
         total_in_toks = 0
         total_out_toks = 0
+
+        if profiling == True:
+            print("=== Initializing profiler ====")
+            lprofiler = profiler.profile(
+                schedule=profiler.schedule(wait=0, warmup=1, active=4, repeat=1),
+                activities=[profiler.ProfilerActivity.CPU, profiler.ProfilerActivity.HPU],
+                on_trace_ready=profiler.tensorboard_trace_handler('vllm_logs', use_gzip=True), with_stack=True, with_modules=False, record_shapes=False, profile_memory=False)
+            lprofiler.start()
+
+
         while self.llm_engine.has_unfinished_requests():
             step_outputs = self.llm_engine.step()
+            if profiling == True:
+                lprofiler.step()
             for output in step_outputs:
                 if output.finished:
                     outputs.append(output)
@@ -1412,7 +1427,8 @@ class LLM:
                                 f"est. speed input: {in_spd:.2f} toks/s, "
                                 f"output: {out_spd:.2f} toks/s")
                         pbar.update(1)
-
+        if profiling == True:
+            lprofiler.stop()
         if use_tqdm:
             pbar.close()
 
