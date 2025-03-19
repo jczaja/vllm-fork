a22087b9929878158c796e367ce78cdaec60e08a
Jacek Czaja <jczaja@habana.ai>
2025-03-19 16:02:13 +0200

    Moved set_indices_and_offsets into prepare_prompt and prepare_decode
    set_indices_and_offsets on CPU
    
    Draft of profiler
    
    Indices and offsets are now on HPU - fix
    
    Revert "Draft of profiler"
    
    This reverts commit dc5d19247ee0db9cfb3bbaff7f9c2031fcd00a0a.
    
    - cleaning up
    
    - removed redundant line

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -391,17 +391,6 @@ class HpuModelAdapter:
         metadata = metadata._replace(block_scales=block_scales)
         return metadata
 
-    def _set_indices_and_offsets(self, metadata, block_size, is_prompt):
-        slot_mapping = metadata.slot_mapping.flatten()
-        indices = torch.div(slot_mapping, block_size, rounding_mode="floor")
-        if is_prompt:
-            indices = indices.unflatten(0, (-1, block_size))[:, 0]
-            offsets = None
-        else:
-            offsets = torch.fmod(slot_mapping, block_size)
-        metadata = metadata._replace(block_offsets=offsets,
-                                     block_indices=indices)
-        return metadata
 
     def _update_metadata(self, attn_metadata, batch_size, seq_len, device,
                          dtype):
@@ -413,9 +402,6 @@ class HpuModelAdapter:
             attn_metadata = self._set_block_mapping(attn_metadata, batch_size,
                                                     device, dtype)
             attn_metadata = self._set_block_scales(attn_metadata, device)
-        attn_metadata = self._set_indices_and_offsets(attn_metadata,
-                                                      self.block_size,
-                                                      attn_metadata.is_prompt)
         return attn_metadata
 
     def _prepare_cos_sin(self, positions):
@@ -1233,11 +1219,11 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             self.device, non_blocking=True)
         input_positions = input_positions.to(  # type: ignore
             self.device, non_blocking=True)
-        slot_mapping = slot_mapping.to(  # type: ignore
-            self.device, non_blocking=True)
+        slot_mapping_HPU = slot_mapping.to(self.device, non_blocking=True)
         seq_lens_tensor = seq_lens_tensor.to(self.device, non_blocking=True)
         context_lens_tensor = context_lens_tensor.to(self.device,
                                                      non_blocking=True)
+
 
         attn_metadata = self.attn_backend.make_metadata(
             is_prompt=True,
@@ -1255,16 +1241,21 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             num_prefills=real_num_seqs,
             num_prefill_tokens=num_prefill_tokens,
             num_decode_tokens=0,
-            slot_mapping=slot_mapping,
+            slot_mapping=slot_mapping_HPU,
             multi_modal_placeholder_index_maps=placeholder_index_maps,
             enable_kv_scales_calculation=False,
         )
+        # calculate indices using CPU and locate them on HPU
+        slot_mapping = slot_mapping.flatten()
+        indices = torch.div(slot_mapping, self.block_size, rounding_mode="floor")
+        indices = indices.unflatten(0, (-1, self.block_size))[:, 0]
+        attn_metadata.block_indices=indices.to(self.device, non_blocking=True)
+
         multi_modal_kwargs = MultiModalKwargs.batch(multi_modal_kwargs_list)
         for t in multi_modal_kwargs:
             if torch.is_tensor(multi_modal_kwargs[t]):
                 multi_modal_kwargs[t] = multi_modal_kwargs[t].to(
                     self.device, non_blocking=True)
-
         return PreparePromptMetadata(input_tokens=input_tokens_tensor,
                                      input_positions=input_positions,
                                      attn_metadata=attn_metadata,
@@ -1274,7 +1265,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                                      lora_prompt_mapping=lora_prompt_mapping,
                                      lora_requests=lora_requests,
                                      multi_modal_kwargs=multi_modal_kwargs,
-                                     slot_mapping=slot_mapping,
+                                     slot_mapping=slot_mapping_HPU,
                                      lora_ids=lora_ids)
 
     def _prepare_decode(
@@ -1497,7 +1488,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
         block_usage = torch.tensor(block_usage,
                                    dtype=self.model_config.dtype,
                                    device='cpu')
-        slot_mapping = torch.tensor(slot_mapping,
+        slot_mapping_CPU = torch.tensor(slot_mapping,
                                     dtype=torch.long,
                                     device='cpu')
 
@@ -1511,7 +1502,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             self.device, non_blocking=True)
         block_usage = block_usage.to(  # type: ignore
             self.device, non_blocking=True)
-        slot_mapping = slot_mapping.to(  # type: ignore
+        slot_mapping_HPU = slot_mapping_CPU.to(  # type: ignore
             self.device, non_blocking=True)
         if is_enc_dec_model:
             cross_block_list = cross_block_list.to(  # type: ignore
@@ -1543,17 +1534,24 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             num_prefills=0,
             num_prefill_tokens=0,
             num_decode_tokens=num_decode_tokens,
-            slot_mapping=slot_mapping,
+            slot_mapping=slot_mapping_HPU,
             multi_modal_placeholder_index_maps=None,
             enable_kv_scales_calculation=False,
         )
+        # calculate indices and offsets using CPU and locate them on HPU
+        slot_mapping = slot_mapping_CPU.flatten()
+        indices = torch.div(slot_mapping, self.block_size, rounding_mode="floor")
+        offsets = torch.fmod(slot_mapping, self.block_size)
+        attn_metadata.block_offsets=offsets.to(self.device, non_blocking=True)
+        attn_metadata.block_indices=indices.to(self.device, non_blocking=True)
+
         return PrepareDecodeMetadata(input_tokens=input_tokens,
                                      input_positions=input_positions,
                                      attn_metadata=attn_metadata,
                                      lora_index_mapping=lora_index_mapping,
                                      lora_prompt_mapping=lora_prompt_mapping,
                                      lora_requests=lora_requests,
-                                     slot_mapping=slot_mapping,
+                                     slot_mapping=slot_mapping_HPU,
                                      lora_ids=lora_ids)
 
     def prepare_input_tensors(
